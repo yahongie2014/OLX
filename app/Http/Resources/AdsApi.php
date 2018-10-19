65eb6643966c754e58156a80094f3ff2178e0f12
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-19 02:58:42 +0200

    Heroku

--- app/Http/Resources/AdsApi.php
+++ app/Http/Resources/AdsApi.php
@@ -14,6 +14,6 @@ class AdsApi extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [];
     }
 }
--- app/Http/Resources/BankApi.php
+++ app/Http/Resources/BankApi.php
@@ -14,6 +14,6 @@ class BankApi extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return[];
     }
 }
