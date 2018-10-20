1dd270d4e0c021d3ed277aaaaf24f6f7c8a9ad52
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-21 00:26:00 +0200

    storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,8 +51,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'visibility' => 'public',
             'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
         ],
 
         's3' => [
