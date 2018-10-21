ad5eac646b7d3997fb4dcd70276409654dc5863d
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-21 12:27:59 +0200

    Products Update

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -158,66 +158,108 @@ class ProductsController extends Controller
      * @param  \App\Models\Products $products
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request)
+    protected function update(Request $request, $id)
     {
-        $updates = $this->products->findOrFail($request->id);
+        $data = $request->all();
 
-        if ($request->user()->id !== $updates) {
-            return response()->json(['error' => 'You can only edit your own Product.'], 403);
-        }
-
-        $product = new $this->products($request->all());
-        $file_cover = $request->file('cover_image');
-        $ext_cover = $file_cover->getClientOriginalExtension();
-        $cover = Storage::putFileAs('/public/Products', $file_cover, $ext_cover);
-        if ($cover) {
-            $product::find($product->id);
-            $product->cover_image = $ext_cover;
-            $product->user_id = $request->user()->id;
-            $product->update();
-        }
-
-        if ($product->save()) {
-            $pictures = new $this->images();
-            $file = $request->file('path');
-            $ext = $file->getClientOriginalExtension();
-            $name = Storage::putFileAs('/public/FeaturesProduct', $file, $ext);
-            if ($name) {
-                $pictures::create([
-                    'path' => $ext,
-                    'products_id' => $product->id
-                ]);
+        if ($request->global_locale_ar) {
+            $rules = array(
+                'name_ar' => 'required|string|max:50',
+                'desc_ar' => 'required|string|max:255',
+                'locale_ar' => 'required',
+            );
+            $messages = array(
+                'name_ar.required' => 'Arabic Name Must be Required',
+                'desc_ar.required' => 'Arabic Descereption Must be Required',
+            );
+            $validator = \Validator::make(Input::all(), $rules, $messages);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()->all()])->setStatusCode(422);
             }
-            if ($request->locale_ar) {
-                $rules = array(
-                    'name_ar' => 'required|string|max:50',
-                    'desc_ar' => 'required|string|max:255',
-                    'price_ar' => 'required|integer',
-                    'locale_ar' => 'required',
-                );
-                $messages = array(
-                    'name_ar.required' => 'Arabic Name Must be Required',
-                    'desc_ar.required' => 'Arabic Descereption Must be Required',
-                    'price_ar.required' => 'Arabic Price Must be Required',
-                    'price_ar.integer' => 'Arabic Price arabic Must be Number',
-                );
-                $validator = \Validator::make(Input::all(), $rules, $messages);
-                if ($validator->fails()) {
-                    return response()->json(['errors' => $validator->errors()->all()])->setStatusCode(422);
+        }
+        if ($request->global_locale) {
+            $rules = array(
+                'name' => 'required|string|max:50',
+                'desc' => 'required|string|max:255',
+                'price' => 'required',
+                'is_active' => 'required|integer',
+            );
+            $messages = array(
+                'name.required' => 'Name Must be Required',
+                'desc.required' => 'Descereption Must be Required',
+                'price.required' => 'Descereption Must be Required',
+                'is_active.required' => 'Status Must be Required',
+            );
+            $validator = \Validator::make(Input::all(), $rules, $messages);
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()->all()])->setStatusCode(422);
+            }
+        }
+
+
+        DB::beginTransaction();
+        $update = $this->products->find($id);
+        if ($request->has('name')) {
+            $update->name = $request->name;
+        }
+        if ($request->has('desc')) {
+            $update->desc = $request->desc;
+        }
+        if ($request->has('price')) {
+            $update->price = $request->price;
+        }
+        if ($request->has('locale')) {
+            $update->locale = $request->global_locale;
+        }
+        if ($request->has('name_ar')) {
+            $update->name = $request->name_ar;
+        }
+        if ($request->has('desc_ar')) {
+            $update->desc = $request->desc_ar;
+        }
+        if ($request->has('price_ar')) {
+            $update->price = $request->price_ar;
+        }
+        if ($request->has('locale')) {
+            $update->locale = $request->global_locale_ar;
+        }
+        if ($request->has('is_active')) {
+            $update->is_active = $request->is_active;
+        }
+        if ($request->has('cover_image')) {
+            $file_cover = $request->file('cover_image');
+            $name_cover = $file_cover->getClientOriginalName();
+            $ext_cover = $file_cover->getClientOriginalExtension();
+            $cover = Storage::putFileAs('/public/Products', $file_cover, $name_cover);
+            $update->cover_image = $name_cover;
+        }
+        if ($update->update()) {
+            if ($request->has('Images')) {
+                $file = $request->has('Images');
+                foreach ($file as $files) {
+                    $idPRo = $this->images->find("product_id",$update->id);
+                    foreach ($idPRo as $id => $value) {
+                        $productImage = $request->path;
+                        if ($productImage) {
+                            $delete_old = Storage::delete('/public/FeaturesProduct' . $productImage);
+                            $update->destroy($id);
+                        }
+                    }
+                    $name_image = $files->getClientOriginalName();
+                    $ext = $files->getClientOriginalExtension();
+                    $name = Storage::putFileAs('/public/FeaturesProduct', $files, $name_image);
+                    $img = new $this->images($request->all());
+                    $img->products_id = $update->id;
+                    $img->path = $name_image;
+                    $img->update();
                 }
-                $pro_trnslator = new $this->trnslator();
-                $pro_trnslator->name = $request->name_ar;
-                $pro_trnslator->desc = $request->desc_ar;
-                $pro_trnslator->price = $request->price_ar;
-                $pro_trnslator->locale = $request->locale_ar;
-                $pro_trnslator->products_id = $product->id;
-                $pro_trnslator->save();
             }
-            DB::commit();
 
         }
+        DB::commit();
 
-        return new ProductsApi($product);
+
+        return new ProductsApi($update);
 
     }
 
