060a1ea246bf1f9c4ef05858b8ef7e6f835d2763
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-22 07:36:15 +0200

    try again cache

--- config/cache.php
+++ config/cache.php
@@ -57,19 +57,6 @@ return [
                 env('MEMCACHED_PASSWORD'),
             ],
             'options' => [
-                Memcached::OPT_TCP_NODELAY => TRUE,
-                Memcached::OPT_NO_BLOCK => FALSE,
-                // - timeouts
-                Memcached::OPT_CONNECT_TIMEOUT => 2000,    // ms
-                Memcached::OPT_POLL_TIMEOUT => 2000,       // ms
-                Memcached::OPT_RECV_TIMEOUT => 750 * 1000, // us
-                Memcached::OPT_SEND_TIMEOUT => 750 * 1000, // us
-                // - better failover
-                Memcached::OPT_DISTRIBUTION => Memcached::DISTRIBUTION_CONSISTENT,
-                Memcached::OPT_LIBKETAMA_COMPATIBLE => TRUE,
-                Memcached::OPT_RETRY_TIMEOUT => 2,
-                Memcached::OPT_SERVER_FAILURE_LIMIT => 1,
-                Memcached::OPT_AUTO_EJECT_HOSTS => TRUE,
                 ],
             'servers' => array_map(function($s) {
                 $parts = explode(":", $s);
