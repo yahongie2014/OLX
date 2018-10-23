5639dc5fc599826d519abf810de5df79ad3e3be6
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-23 23:38:34 +0200

    Finally

--- app/Http/Resources/OrdersApi.php
+++ app/Http/Resources/OrdersApi.php
@@ -7,6 +7,7 @@ use App\Models\OrderItmes;
 use App\Models\Products;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class OrdersApi extends JsonResource
 {
@@ -18,6 +19,19 @@ class OrdersApi extends JsonResource
      */
     public function toArray($request)
     {
+        $orders_d = OrderItmes::select("*")
+            ->leftjoin("products", "products.id", "=", "order_items.product_id")
+            ->where("products.user_id", $request->user()->id)
+            ->where("order_items.order_id", $this->id)
+            ->get();
+
+        $orders = OrderItmes::select("*")
+            ->leftjoin("products", "products.id", "=", "order_items.product_id")
+            ->where("products.user_id", $request->user()->id)
+            ->sum(DB::raw('order_items.price * order_items.qty'));
+        $percent = $orders * env("PERCENTAGE") / 100;
+
+
         return [
             "Identifier" => $this->id,
             "WebSiteName" => env('APP_NAME', 'At Time'),
@@ -28,13 +42,9 @@ class OrdersApi extends JsonResource
             "CustomerImage" => $this->user->image,
             "OrderNumber" => "#" . $this->order_number,
             "PromoCode" => (boolean)$this->promo_code_id,
-            "OrderItems" => OrdersItemsApi::collection(OrderItmes::with(["Products" => function ($q) {
-                $q->select("products.id");
-                $q->groupBy("products.id");
-                $q->where("products.user_id", Auth::user()->id);}])
-                ->where("order_id", $this->id)
-                ->whereNotNull("price")
-                ->get()),
+            "OrderItems" => OrdersItemsApi::collection($orders_d),
+            "TotalProfit" => $orders,
+            "WebsiteMustPaid" => $percent,
             "PercentageWebsite" => env("PERCENTAGE") . "%",
         ];
 
