c2e344a31db1d39d021b78ed8e3557016ad43f87
Coder 79 <dev.ahmed.saeed@gmail.com>
2018-10-28 14:31:14 +0200

    enable email duplicate

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -41,7 +41,7 @@ class AuthController extends Controller
 
         $request->validate([
             'name' => 'required|string',
-            'email' => 'required|string|email|unique:users',
+//            'email' => 'required|string|email|unique:users',
             'phone' => 'required',
             'longitude' => 'required',
             'latitudes' => 'required',
